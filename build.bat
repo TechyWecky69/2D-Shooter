07691f0539e0a2ca913570e5d103249693b1bde5
TechyWecky69 <techyisontop@gmail.com>
2024-11-29 16:48:01 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 pip install pyinstaller
 pip install pygame
-pyinstaller --noconfirm --onefile --windowed --name "2D Shooter"  "C:\Users\Ethan\PycharmProjects\PythonProject\main.py"
+pyinstaller --noconfirm --onefile --windowed --name "2D Shooter"  "./main.py"
